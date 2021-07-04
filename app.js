693013e831208677462555746cd9a5dc6270fd62
Jose Chaparro <josechaparro@Joses-MacBook-Pro.local>
2021-07-04 14:07:31 -0700

    macbook section html and css done. JS functionality for the scroll event done. Macbook effects added for the opening and displaying of info

--- app.js
+++ app.js
@@ -104,3 +104,11 @@ const slideshow = () => {
 slideshow();
 
 
+// Section 3
+const section3Content = document.querySelector('.section-3-content')
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset + window.innerHeight >= section3Content.offsetTop + section3Content.offsetHeight / 2) {
+        section3Content.classList.add('change');
+    }
+});
